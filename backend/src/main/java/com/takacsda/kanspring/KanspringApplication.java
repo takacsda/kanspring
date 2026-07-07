fba521f336006e2f268e9732194ea0c4c12595dc
Takács Dániel <dtakacs.inf@proton.me>
2026-07-07 18:48:11 +0200

    feat: init repo

---
+++ backend/src/main/java/com/takacsda/kanspring/KanspringApplication.java
@@ -0,0 +1,13 @@
+package com.takacsda.kanspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanspringApplication.class, args);
+	}
+
+}
